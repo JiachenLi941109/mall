1521fc76af7906a4bc273eee6b27f7ea8b6d07dd
JiachenLi941109 <wersd_qazse@163.com>
2021-10-17 23:03:35 +0800

    update

--- src/main/java/com/jiachen/mall/dao/UmsAdminRoleRelationDao.java
+++ src/main/java/com/jiachen/mall/dao/UmsAdminRoleRelationDao.java
@@ -3,6 +3,7 @@ package com.jiachen.mall.dao;
 import com.jiachen.mall.mbg.model.UmsPermission;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * Created by macro on 2018/10/8.
  */
 @Mapper
+@Repository
 public interface UmsAdminRoleRelationDao {
 
     /**
